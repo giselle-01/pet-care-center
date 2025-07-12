45bb33dac82a6eb6beb7eaebb8a10392f3167edf
giselle-01 <giselleandrea027@gmail.com>
2025-07-11 19:43:55 -0500

    Feat: The structure was created.

---
+++ src/js/dashboard.js
@@ -0,0 +1,18 @@
+const $form = document.getElementById("register-form")
+const $name = document.getElementById("register-name");
+const $userName = document.getElementById("register-username");
+const $email = document.getElementById("register-email");
+const $phone = document.getElementById("register-phone");
+const $password = document.getElementById("register-password");
+
+const $btnRegister = document.getElementById("btn-register");
+
+const userData = {
+    name: $name.value,
+    userName: $userName.value,
+    email: $email.value,
+    phone: $phone.value,
+    password: $password.value
+};
+
+
